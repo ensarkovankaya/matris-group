cf5cf057777a3c3b1349108302f3911ecd2205b9
Ensar Kovankaya <ensar@kovankaya.com>
2018-08-22 23:27:58 +0300

    add packages

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -22,12 +22,16 @@ else
     # If environment development use nodemon to watch file changes
     # Otherwise just run the server
     if [ "$NODE_ENV" == "development" ] || [ "$NODE_ENV" == "dev" ]; then
-        npm run watch
-    else
+        npm run start:dev
+    elif [ "$NODE_ENV" == "test" ]; then
+        npm run start:test
+    elif [ "$NODE_ENV" == "production" ] || [ "$NODE_ENV" == "prod" ]; then
         # Install dependencies if node_modules not exists
         if [ ! -d "node_modules" ]; then
             install
         fi
-        npm run start
+        npm run start:prod
+    else
+        echo 'Unknown environment' && exit 1
     fi
 fi
