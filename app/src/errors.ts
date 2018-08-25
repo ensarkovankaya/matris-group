fa782e0d6c95389a50d6fe50f7f37743930491dd
Ensar Kovankaya <ensar@kovankaya.com>
2018-08-25 17:29:30 +0300

    add errors

---
+++ app/src/errors.ts
@@ -0,0 +1,20 @@
+/**
+ * If function or method argument not satisfied curten conditions
+ */
+export class InvalidArgument extends Error {
+    public name = 'InvalidArgument';
+    public argument: string;
+
+    constructor(argument: string, message?: string) {
+        super(message);
+        this.argument = argument;
+    }
+}
+
+export class GroupNotFound extends Error {
+    public name = 'GroupNotFound';
+}
+
+export class UserAlreadyInGroup extends Error {
+    public name = 'UserAlreadyInGroup';
+}
