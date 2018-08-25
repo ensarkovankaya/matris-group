c3a40fda8632d02818301648354ef2cc5b584c12
Ensar Kovankaya <ensar@kovankaya.com>
2018-08-25 17:27:19 +0300

    add generateRandomString function

--- app/src/utils.ts
+++ app/src/utils.ts
@@ -15,3 +15,15 @@ export const isProduction = (): boolean => {
     const env = process.env.NODE_ENV.toLowerCase();
     return env === 'prod' || env === 'production';
 };
+
+/**
+ * Generates random string with given length
+ * @param length String length
+ */
+export const generateRandomString = (length: number): string => {
+    let string = "";
+    while (string.length < length) {
+        string += (Math.random() + 1).toString(36).substring(7);
+    }
+    return string.slice(0, length);
+};
